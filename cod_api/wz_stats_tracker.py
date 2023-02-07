62f41584535dc819b3766a6a0018f287a92d51d2
David <david.besonen139@gmail.com>
2023-02-07 13:18:13 -0600

    Made wz_stats_tracker.py

--- cod_api/wz_stats_tracker.py
+++ cod_api/wz_stats_tracker.py
@@ -1,26 +1,22 @@
-from cod_api import 
+from cod_api import API, platforms
 import asyncio
 
 # Path: cod_api/wz_stats_tracker.py
 api = API()
 api.login('MTU0Mzg2Njc0MjQwNjQ5NDA0MjoxNjc3MDA0OTUwNTkxOjI0YjEyYjMyN2JjMmQ1MDhkMDNiY2NiYzg2Njg0ZDk2')
 
-# retrieving combat history
-profile = api.Warzone.fullData(platforms.Battlenet, "Username#1234") # returns data of type dict
-
-# printing results to console
-print(profile)
-
-## async
+# async
 # in an async function
 async def example():
     # login in with sso token
-    await api.loginAsync('your_sso_token')
+    await api.loginAsync('MTU0Mzg2Njc0MjQwNjQ5NDA0MjoxNjc3MDA0OTUwNTkxOjI0YjEyYjMyN2JjMmQ1MDhkMDNiY2NiYzg2Njg0ZDk2')
 
     # retrieving combat history
-    profile = await api.Warzone.fullDataAsync(platforms.Battlenet, "Username#1234") # returns data of type dict
+    profile = await api.Warzone2.fullDataAsync(platforms.Battlenet, "Magnumox#1645") # returns data of type dict
 
     # printing results to console
     print(profile)
+
+asyncio.run(example())
 
 # CALL THE example FUNCTION IN AN ASYNC ENVIRONMENT
